d76075e9eaf5574b9e8b2ff2fb8b35768ad9da13
juliana-vieira <julianasalustianovieira@gmail.com>
2024-11-15 14:00:45 -0300

    Script do pipeline refatorado para se adaptar a multiplas base de dados JSON.

--- scripts/extract_and_save_data.py
+++ scripts/extract_and_save_data.py
@@ -1,16 +1,49 @@
 from processamento import MongoDatabase, Data
 
-# Conectando ao Mongodb Atlas
-mongo_cluster = MongoDatabase("julianasvieira", "Cluster-pipeline")
+config = {
+    "username" : "julianasvieira",
+    "cluster_name" : "Cluster-pipeline",
+    "url_api" : "https://labdados.com/produtos",
+    "db_name" : "teste",
+    "collection_name" : "colecao-teste-04"
+}
+
+# Conectando no MongoDB Atlas
+mongo_cluster = MongoDatabase(config["username"], config["cluster_name"])
 mongo_cluster.connectMongo()
 
-# Criando o db e a collection
-db = mongo_cluster.get_db("teste")
-collection = mongo_cluster.get_collection(db, "colecao-teste")
+# Criando o banco de dados
+db = mongo_cluster.get_db(config["db_name"])
+collection = mongo_cluster.get_collection(db, config["collection_name"])
 
-# Extraindo os dados da API e inserindo no db
+# Inserindo os dados no banco
 dados = Data()
-dados.extract_api_data("https://labdados.com/produtos")
-dados.insert_data(collection)
-print(dados.consult_data(collection))
+dados.extract_api_data(config["url_api"])
+dados.insert_data_db(collection)
+
+# Extraindo informações sobre os índices da base de dados
+nomes_index = dados.get_index_names(collection)
+print(f"Nomes dos índices: {nomes_index}\n")
+
+# Transformando os dados - renomeando as colunas
+dados.rename_column(collection, "lat", "Latitude")
+dados.rename_column(collection, "lon", "Longitude")
+nomes_index = dados.get_index_names(collection)
+print(f"Nomes dos índices: {nomes_index}\n")
+
+# Extraindo os dados solicitados
+categoria_livros = dados.select_items(collection, "string", "Categoria do Produto", "livros")
+dados_filtrados = dados.select_items(collection, "regex", "Data da Compra", "/202[1-9]")
 mongo_cluster.close_connection()
+
+# Convertendo para dataframe
+df_livros = dados.to_dataframe(categoria_livros)
+df_2021_em_diante = dados.to_dataframe(dados_filtrados)
+
+# Formatando a data para datetime
+df_formatados = {"df_livros" : dados.format_date(df_livros, "Data da Compra"),
+                 "df_2021_em_diante": dados.format_date(df_2021_em_diante, "Data da Compra")}
+
+# Salvando os dados
+for key, item in df_formatados.items():
+     dados.save_csv(item, f"data/{key}.csv")
--- scripts/processamento.py
+++ scripts/processamento.py
@@ -1,8 +1,9 @@
 import requests
+import pandas as pd
+import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
@@ -13,8 +14,8 @@ class MongoDatabase:
         self.cluster_url =  os.getenv("CLUSTER_URL")
 
         if not self.db_password or not self.cluster_url:
-            raise ValueError("DB_PASSWORD or CLUSTER_URL not found in environment variables.")
-
+            raise ValueError("Environment variables not found in .env file.")
+        
         self.uri = f"mongodb+srv://{username}:{self.db_password}@{self.cluster_url}/?retryWrites=true&w=majority&appName={cluster_name}"
         self.client = None
     
@@ -25,14 +26,14 @@ class MongoDatabase:
         # Send a ping to confirm a successful connection
         try:
             self.client.admin.command('ping')
-            print("Conexão bem sucedida!")
+            print("Information: You've connected to Mongo Atlas successfully!")
         except Exception as e:
             print(e)
 
     def get_db(self, db_name):
 
-        if self.client is None:
-            self.connectMongo()
+        if not self.client:
+            raise ValueError("The connection with Mongo Atlas was not established. Please connect to Mongo Atlas before creating a database.")
 
         return self.client[db_name]
     
@@ -47,8 +48,10 @@ class MongoDatabase:
             try:
                 self.client.close()
                 print("MongoDB connection closed.")
+
             except Exception as e:
                 print(f"Error closing connection: {e}")
+
             finally:
                 self.client = None
         else:
@@ -62,25 +65,129 @@ class Data:
     def extract_api_data(self, endpoint):
 
         try:
-            response = requests.get(endpoint)
-            self.data = response.json()
+            response = requests.get(endpoint,timeout=10)
+            response.raise_for_status()
+
+            try:
+                self.data = response.json()
+                
+                if not self.data:
+                    raise ValueError("A API retornou um JSON vazio.")
+                
+                print("Data extracted successfully from the API!")
+
+            except ValueError as json_error:
+                print(f"Erro ao processar JSON da API: {json_error}")
+                self.data = None
+
+        except requests.exceptions.RequestException as req_error:
+            print(f"Erro na solicitação da API: {req_error}")
+            self.data = None
+
+    def insert_data_db(self, collection):
+            
+        if self.data is None:
+            raise ValueError("No data was extracted.")
+        
+        elif collection.count_documents({}) > 0:
+            print("Information: This collection is already populated. If you need to insert more data, please create another collection.")
+            return
+        
+        docs = collection.insert_many(self.data)
+        print("The data was inserted successfully!")
+
+        return docs
+    
+    @staticmethod
+    def get_index_names(collection):
+
+        try:
+            agg = [
+                {"$project": {"keys": {"$objectToArray": "$$ROOT"}}},
+                {"$unwind": "$keys"},
+                {"$group": {"_id": None, "distinctKeys": {"$addToSet": "$keys.k"}}},
+            ]
+
+            result = list(collection.aggregate(agg))
+
+            return result[0]["distinctKeys"]
+        
+        except Exception as e:
+            print(e)
+    
+    @staticmethod
+    def get_collection_data(collection):
+
+        doc_list = []
+
+        try:
+            for doc in collection.find():
+                doc_list.append(doc)
+
+            return doc_list
+        
+        except Exception as e:
+            print(e)
+
+    def rename_column(self, collection, column_name, new_name):
+        try:
+            
+            if new_name in self.get_index_names(collection):
+
+                print(f'The column name "{new_name}" is already present in the database.')
+                return
+            else:
+
+                collection.update_many({}, {"$rename" : {column_name : new_name}})
+            print(f'Column renamed successfully from "{column_name}" to "{new_name}"')
 
         except Exception as e:
             print(e)
 
-    def insert_data(self, collection):
-            
-        if self.data is None:
-            raise ValueError("Nenhum dado foi extraído para inserir.")
-            
-        docs = collection.insert_many(self.data)
-        return docs
+    @staticmethod
+    def select_items(collection, query_type, field, item):
+
+        result = []
+
+        try:
+            if query_type == "regex":
+                query = {field : {"$regex" : item}}
+
+            elif query_type == "string":
+                query = {field : item}
+
+            for doc in collection.find(query):
+                result.append(doc)
+
+            return result
+        
+        except Exception as e:
+            print(e)
     
     @staticmethod
-    def consult_data(collection):
+    def to_dataframe(df):
+
+        df = pd.DataFrame(df)
+
+        return df
+    
+    @staticmethod
+    def format_date(df, column_date):
+
         try:
-            query = collection.find_one()
-            return query
-        
+            df[column_date] = pd.to_datetime(df[column_date], format = "%d/%m/%Y")
+            print("Date formatted successfully.")
+            return df
+
+        except Exception as e:
+            print(e)
+
+    @staticmethod
+    def save_csv(df, path):
+
+        try:
+            df.to_csv(path)
+            print("Data saved successfully as a csv file.")
+
         except Exception as e:
             print(e)
